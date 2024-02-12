a3ed3ce35d0efc2f584b81b5d27f90ef5445b2ef
adrianhajdin <contact@jsmastery.pro>
2024-02-12 17:08:27 +0100

    Update publicRoutes in authMiddleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ['/api/webhooks/clerk']
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
 });
  
 export const config = {
